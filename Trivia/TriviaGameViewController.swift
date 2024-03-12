570bfd69f7da7f17f734eaa4c2589894f82cf373
nateloading17 <nathanmilobaez@gmail.com>
2024-03-11 21:19:18 -0400

    2024 Oscar Trivia

---
+++ Trivia/TriviaGameViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TriviaGameViewController.swift
+//  Trivia
+//
+//  Created by Nathaniel Baez on 3/11/24.
+//
+
+import SwiftUI
+
+struct TriviaGameViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TriviaGameViewController()
+}
